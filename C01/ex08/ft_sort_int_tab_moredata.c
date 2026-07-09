8d5d43da9cbd1d36fca3656de03aecc312ddeb19
Ronnakon <kamolkluaykaeo@gmail.com>
2026-07-09 16:13:32 +0700

    fix ex08

--- C01/ex08/ft_sort_int_tab_moredata.c
+++ C01/ex08/ft_sort_int_tab_moredata.c
@@ -5,31 +5,67 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ronny <ronny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/09 16:09:18 by ronny             #+#    #+#             */
-/*   Updated: 2026/07/09 16:12:50 by ronny            ###   ########.fr       */
+/*   Created: 2026/07/09 16:01:49 by ronny             #+#    #+#             */
+/*   Updated: 2026/07/09 16:03:26 by ronny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 void	ft_sort_int_tab(int *tab, int size)
 {
+	int	temp[size];
 	int	i;
-	int	j;
-	int	tmp;
+	int	c;
+	int	cc;
+	int	max;
 
 	i = 0;
-	while (i < size - 1)
+	while (i < size)
 	{
-		j = i + 1;
-		while (j < size)
-		{
-			if (tab[i] > tab[j])
-			{
-				tmp = tab[i];
-				tab[i] = tab[j];
-				tab[j] = tmp;
-			}
-			j++;
-		}
+		temp[i] = tab[i];
 		i++;
 	}
+	c = size - 1;
+	while (c >= 0)
+	{
+		i = size - 1;
+		max = temp[0];
+		while (i >= 0)
+		{
+			if (temp[i] > max)
+				max = temp[i];
+			i--;
+		}
+		cc = size - 1;
+		while (cc >= 0)
+		{
+			if (temp[cc] == max)
+			{
+				temp[cc] = '\0';
+				break ;
+			}
+			cc--;
+		}
+		tab[c] = max;
+		c--;
+	}
 }
+
+// int	main(void)
+// {
+// 	int	s[] = {1, 2, 5, 4, 8,1,2,1};
+// 	int	i;
+
+// 	ft_sort_int_tab(s, 8);
+
+// 	i = 0;
+// 	while (i < 8)
+// 	{
+// 		printf("%d ", s[i]);
+// 		i++;
+// 	}
+// 	printf("\n");
+
+// 	return (0);
+// }
