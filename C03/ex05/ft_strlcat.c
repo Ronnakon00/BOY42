cfb18be5ffe84fae80ad199bed04ac372cb77b23
Ronnakon Thanetthammakhun <rthanett@z1t5c4.42bangkok.com>
2026-07-14 14:21:23 +0700

    fix

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,35 +6,46 @@
 /*   By: rthanett <rthanett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 15:38:04 by rthanett          #+#    #+#             */
-/*   Updated: 2026/07/13 22:50:55 by rthanett         ###   ########.fr       */
+/*   Updated: 2026/07/14 13:47:08 by rthanett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+#include <stdio.h>
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	dest_len;
 	unsigned int	src_len;
 	unsigned int	i;
-	unsigned int	j;
 
 	dest_len = 0;
-	while (dest_len < size && dest[dest_len] != '\0')
+	while (dest[dest_len] != '\0')
 		dest_len++;
 	src_len = 0;
 	while (src[src_len] != '\0')
 		src_len++;
 	if (size <= dest_len)
 		return (size + src_len);
-	i = dest_len;
-	j = 0;
-	while (src[j] != '\0' && i < (size - 1))
+	i = 0;
+	while (src[i] != '\0' && (dest_len + i) < (size - 1))
 	{
-		dest[i] = src[j];
+		dest[(i + dest_len)] = src[i];
 		i++;
-		j++;
 	}
-	dest[i] = '\0';
-	return (dest_len + src_len);
+	dest[dest_len + i] = '\0';
+	return (src_len + dest_len);
 }
+
+// int main ()
+// {
+// 	char dst[] = "Hi ";
+// 	char srt[] = "My Friend";
+// 	unsigned x = ft_strlcat(dst,srt,8);
+// 	printf("%d\n",x);
+// 	int i=0;
+// 	while(dst[i] != '\0')
+// 	{
+// 		printf("%c",dst[i]);
+// 		i++;
+// 	}
+// }
