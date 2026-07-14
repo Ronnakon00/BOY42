a4303828fff6476c33a1a0681b3dba8042a2b20d
Ronnakon Thanetthammakhun <rthanett@z2t19c2.42bangkok.com>
2026-07-14 21:58:29 +0700

    asdwda

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,15 +6,83 @@
 /*   By: rthanett <rthanett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/14 14:42:56 by rthanett          #+#    #+#             */
-/*   Updated: 2026/07/14 15:58:22 by rthanett         ###   ########.fr       */
+/*   Updated: 2026/07/14 21:41:24 by rthanett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 
-#include <unistd.h>
-void	ft_putnbr(int nb)
+int	power(int i)
 {
-	char c;
-	c = nb + '0';
-	write (1 ,&c, 10);
+	int	aws;
+
+	aws = 1;
+	while (i != 0)
+	{
+		aws = aws * 10;
+		i--;
+	}
+	return (aws);
 }
+
+int	ft_ne(int n)
+{
+	n = n / 2;
+	if (n != 0)
+		return (-1);
+	return (1);
+}
+
+int	len_num(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*str >= '0' && *str <= '9')
+	{
+		i++;
+		str++;
+	}
+	return (i);
+}
+
+int	to_int(char *str, int l)
+{
+	int	result;
+	int	i;
+
+	i = 0;
+	result = 0;
+	while (l != 0)
+	{
+		result = ((str[l] - '0') * power(i)) + result;
+		l--;
+		i++;
+	}
+	return (result);
+}
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	ne;
+	int	i;
+
+	i = 0;
+	result = 0;
+	while (!(str[i] >= '0' && str[i] <= '9'))
+	{
+		if (str[i] == '-')
+			ne++;
+		i++;
+	}
+	result = to_int (&str[i - 1], len_num (&str[i]));
+	return (result * ft_ne(ne));
+}
+
+// int main()
+// {
+//     char c[] = "   ---+--+1234ab567";
+//     int x = ft_atoi(c);
+//     printf("%d",x);
+// }
