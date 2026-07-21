adb6994ed29a6bf50cf842dbb702705d2802a78e
kamolkluaykaeo@gmail.com <kamolkluaykaeo@gmail.com>
2026-07-21 20:27:03 +0700

    rony

--- C07/ex05/ft_split.c
+++ C07/ex05/ft_split.c
@@ -3,11 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ronny <ronny@student.42.fr>                +#+  +:+       +#+        */
+/*   By: rthanett <rthanett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/20 11:57:47 by ronny             #+#    #+#             */
-/*   Updated: 2026/07/20 11:57:54 by ronny            ###   ########.fr       */
+/*   Updated: 2026/07/21 18:09:01 by rthanett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <>
+#include <stdlib.h>
+
+char    **ft_split(char *str, char *charset)
+{
+    int	i;
+	char *start;
+	char len_word;
+	
+    while (*str != '\0')
+    {
+      i = 0;
+	  start = str;
+	  len_word++;
+	  while (charset[i] != '\0')
+	  {
+		if (*str == charset[i])
+			
+	  }
+    }
+}
--- C09/ex00/libft_creator.sh
+++ C09/ex00/libft_creator.sh
@@ -1,3 +1,5 @@
-$bash
+#!/bin/sh
 
 gcc -Wall -Wextra -Werror -c *.c
+ar rcs libft.a *.o
+rm -f *.o
